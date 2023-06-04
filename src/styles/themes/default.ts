ae33f3f17faca8df5a06d9db7315dba3e4702913
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2023-06-04 13:42:23 -0300

    :feat: Finalizando configurações inicial para refatoração do projeto SOS Roterios.

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,36 @@
+export const defaultTheme = {
+  color: {
+    'primary-green-dark': '#3f6f6f',
+    'primary-green-light': '#6fe0be',
+    'secondary-gold': '#ceaa4f',
+    'secondary-blue-light': '#3877B7',
+    'secondary-blue-dark': '#33486F',
+    background: '#EAF8F3',
+    'background-blue-light': '#EFF7F8',
+
+    white: '#FFFFFF',
+    'gray-100': '#DEE3ED',
+    'gray-300': '#8795AF',
+    'gray-500': '#4A576F',
+    'gray-800': '#1F293D',
+  },
+  textSizes: {
+    'title-title-xl': '6rem',
+    'title-title-bold': '3.375rem',
+    'title-title-l': '2rem',
+    'title-title-m': '1.5rem',
+    'title-title-s': '1.25rem',
+
+    'text-regular-large': '1rem',
+    'text-body': '0.875rem',
+    'text-small': '0.75rem',
+    'text-bold-m': '1rem',
+  },
+  fonts: {
+    regular: "'Poppins'",
+    title: "'Open Sans'",
+  },
+  imgbanner: {
+    banner: './assets/baneer.svg',
+  },
+}
