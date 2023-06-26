d2e87eb7ddd924c0092c77a96004877105aec14b
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2023-06-26 11:24:59 -0300

    Criando Component de Roteiros de Viagem.

---
+++ src/pages/TravelItinerary/index.tsx
@@ -0,0 +1,9 @@
+import { TravelItineraryContainer } from './styles'
+
+export const TravelItinerary = () => {
+  return (
+    <TravelItineraryContainer>
+      <h1>Roterio viagems</h1>
+    </TravelItineraryContainer>
+  )
+}
