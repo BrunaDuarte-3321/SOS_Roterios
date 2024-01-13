295f1ab600f753ea17018113b63fa7000acba805
Bruna Duarte <b.elisabete.duarte@parvi.com.br>
2024-01-13 11:16:52 -0300

    feat: excluindo components para restruturação

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,12 @@
 import { ThemeProvider } from 'styled-components'
+
 import { Router } from './router'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/themes/global'
 
-export const Teste = (props: any) => {
-  return (
-    <div>
-      <p>Hello {props.name}</p>
-      <p>{props.children}</p>
-    </div>
-  )
-}
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Teste name="candiate" children="caditate">
-        <p>testando</p>
-      </Teste>
       <Router />
       <GlobalStyle />
     </ThemeProvider>
--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,9 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { TravelItinerary } from './pages/TravelItinerary'
-import { LayoutDefault } from './layouts/DefaultLayout'
-import { Register } from './pages/Register'
-export const Router = () => {
+import { Route, Routes } from 'react-router-dom'
+
+export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<LayoutDefault />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/travelItinerary" element={<TravelItinerary />} />
-        <Route path="/registerLogin" element={<Register />} />
-      </Route>
+      <Route path="/" element={<p>Teste</p>} />
     </Routes>
   )
 }
